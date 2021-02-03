7d13793387865b2f275ec04c83c8d7684017bd70
SBurnt <zip2004zip@gmail.com>
2021-02-03 21:22:51 +0300

    added "personal-information" page

--- assets/js/personal-page.js
+++ assets/js/personal-page.js
@@ -213,7 +213,6 @@ $('#personal__input-card').mask('99999', { autoclear: false });
 
 const sliderPartners = document.querySelector('.js-partners-slider');
 if (sliderPartners) {
-  console.log('slider partners');
   const swiper = new Swiper(sliderPartners, {
     slidesPerView: 1,
     slidesPerGroup: 1,
@@ -224,4 +223,208 @@ if (sliderPartners) {
       prevEl: '.swiper-button-prev',
     },
   });
+}
+
+class Modal {
+  constructor(options) {
+    const defaultOptions = {
+      isOpen: () => {},
+      isClose: () => {},
+    };
+    this.options = Object.assign(defaultOptions, options);
+    this.modal = document.querySelector('.modals');
+    this.speed = false;
+    this.animation = false;
+    this.isOpen = false;
+    this.modalContainer = false;
+    this.previousActiveElement = false;
+    this.fixBlocks = document.querySelectorAll('.fix-block');
+    this.focusElements = ['a[href]', 'input', 'button', 'select', 'textarea', '[tabindex]'];
+    this.events();
+  }
+
+  events() {
+    if (this.modal) {
+      document.addEventListener(
+        'click',
+        function (e) {
+          const clickedElement = e.target.closest('[data-path]');
+          if (clickedElement) {
+            const target = clickedElement.dataset.path;
+            const animation = clickedElement.dataset.animation;
+            const speed = clickedElement.dataset.speed;
+            this.animation = animation ? animation : 'fade';
+            this.speed = speed ? parseInt(speed) : 300;
+            this.modalContainer = document.querySelector(`[data-target="${target}"]`);
+            this.open();
+            return;
+          }
+
+          if (e.target.closest('.modal__close')) {
+            this.close();
+            return;
+          }
+          if (e.target.closest('.personal__btn.cancel')) {
+            this.close();
+            return;
+          }
+          if (e.target.closest('.personal__btn.ok')) {
+            this.close();
+            return;
+          }
+        }.bind(this)
+      );
+
+      window.addEventListener(
+        'keydown',
+        function (e) {
+          if (e.keyCode === 27) {
+            if (this.isOpen) {
+              this.close();
+            }
+          }
+
+          if (e.keyCode === 9 && this.isOpen) {
+            this.focusCatch(e);
+            return;
+          }
+        }.bind(this)
+      );
+
+      this.modal.addEventListener(
+        'click',
+        function (e) {
+          if (
+            !e.target.classList.contains('modal__container') &&
+            !e.target.closest('.modal__container') &&
+            this.isOpen
+          ) {
+            this.close();
+          }
+        }.bind(this)
+      );
+    }
+  }
+
+  open() {
+    this.previousActiveElement = document.activeElement;
+
+    this.modal.style.setProperty('--transition-time', `${this.speed / 1000}s`);
+    this.modal.classList.add('is-open');
+    this.disableScroll();
+
+    this.modalContainer.classList.add('modal__open');
+    this.modalContainer.classList.add(this.animation);
+
+    setTimeout(() => {
+      this.options.isOpen(this);
+      this.modalContainer.classList.add('animate-open');
+      this.isOpen = true;
+      this.focusTrap();
+    }, this.speed);
+  }
+
+  close() {
+    if (this.modalContainer) {
+      this.modalContainer.classList.remove('animate-open');
+      this.modalContainer.classList.remove(this.animation);
+      this.modal.classList.remove('is-open');
+      this.modalContainer.classList.remove('modal__open');
+
+      this.enableScroll();
+      this.options.isClose(this);
+      this.isOpen = false;
+      this.focusTrap();
+    }
+  }
+
+  focusCatch(e) {
+    const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+    const focusArray = Array.prototype.slice.call(focusable);
+    const focusedIndex = focusArray.indexOf(document.activeElement);
+
+    if (e.shiftKey && focusedIndex === 0) {
+      focusArray[focusArray.length - 1].focus();
+      e.preventDefault();
+    }
+
+    if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
+      focusArray[0].focus();
+      e.preventDefault();
+    }
+  }
+
+  focusTrap() {
+    const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+    if (this.isOpen) {
+      focusable[0].focus();
+    } else {
+      this.previousActiveElement.focus();
+    }
+  }
+
+  disableScroll() {
+    const pagePosition = window.scrollY;
+    // this.lockPadding();
+    document.body.classList.add('disable-scroll');
+    document.body.dataset.position = pagePosition;
+    document.body.style.top = -pagePosition + 'px';
+  }
+
+  enableScroll() {
+    const pagePosition = parseInt(document.body.dataset.position, 10);
+    // this.unlockPadding();
+    document.body.style.top = 'auto';
+    document.body.classList.remove('disable-scroll');
+    window.scroll({ top: pagePosition, left: 0 });
+    document.body.removeAttribute('data-position');
+  }
+
+  lockPadding() {
+    const paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+    this.fixBlocks.forEach((el) => {
+      el.style.paddingRight = paddingOffset;
+    });
+    document.body.style.paddingRight = paddingOffset;
+  }
+
+  unlockPadding() {
+    this.fixBlocks.forEach((el) => {
+      el.style.paddingRight = '0px';
+    });
+    document.body.style.paddingRight = '0px';
+  }
+}
+
+const modal = new Modal({
+  isOpen: (modal) => {},
+  isClose: () => {},
+});
+
+const modalEditPerson = function () {
+  const btnClientInfoEdit = document.querySelectorAll('.client-info__btn-edit');
+  const btnPersonalOk = document.querySelector('.personal__btn.ok');
+  const modalInput = document.querySelector('.js-input-modal');
+  let parent = '';
+
+  function setModalInfo(e) {
+    parent.querySelector('.client-info__item-value').textContent = modalInput.value;
+  }
+
+  function getModalInfo(e) {
+    parent = this.closest('.client-info__item');
+    const childValue = parent.querySelector('.client-info__item-value').textContent;
+    modalInput.value = childValue;
+  }
+
+  btnClientInfoEdit.forEach((item) => {
+    item.addEventListener('click', getModalInfo);
+  });
+
+  btnPersonalOk.addEventListener('click', setModalInfo);
+};
+
+const btnClientInfoEdit = document.querySelector('.client-info__btn-edit');
+if (btnClientInfoEdit) {
+  modalEditPerson();
 }
